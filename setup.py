e3e91a5020b155e012466631d317b67da304f07c
mordy <imky171@gmail.com>
2021-07-19 11:59:56 -0400

    added setup

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="code-fetcher",
     version="1.0.3",
-    author="Mordy Waldner",
+    author="Mordy Waldo",
     author_email="imky171@gmail.com",
     description="A package to fetch single files from Github repos instead of cloning the whole thing",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mordy-python/code-fetch",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/mordy-python/code-fetch/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "code_fetch"},
+    packages=setuptools.find_packages(where="code_fetch"),
     python_requires=">=3.6",
 )
