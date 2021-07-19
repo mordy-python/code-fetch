6b88bb1aab6203ecc539e0a23fa0d808cac6f4aa
mordy <imky171@gmail.com>
2021-07-19 12:09:11 -0400

    changed pypi name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="code-fetcher",
+    name="code-fetch",
     version="1.0.3",
     author="Mordy Waldo",
     author_email="imky171@gmail.com",
